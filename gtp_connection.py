4f7eb48eac701969dec19e27aa8c67a2f3a31c5f
Chris Saunders <cs3@ualberta.ca>
2017-02-19 16:49:14 -0700

    Changed for ass 2

--- gtp_connection.py
+++ gtp_connection.py
@@ -2,7 +2,7 @@
 Module for playing games of Go using GoTextProtocol
 
 This code is based off of the gtp module in the Deep-Go project
-by Isaac Henrion and Aamos Storkey at the University of Edinburgh.
+by Isaac Henrion and Amos Storkey at the University of Edinburgh.
 """
 import traceback
 import sys
@@ -11,32 +11,28 @@ from board import GoBoard
 from board_util import GoBoardUtil, BLACK, WHITE, EMPTY, BORDER, FLOODFILL
 import numpy as np
 import re
+import time
 
 class GtpConnection():
 
-    def __init__(self, go_engine,outfile = 'gtp_log', debug_mode = False):
+    def __init__(self, go_engine, debug_mode = False):
         """
         object that plays Go using GTP
 
         Parameters
         ----------
-        go_engine : GoPlayer
+        go_engine: GoPlayer
             a program that is capable of playing go by reading GTP commands
-        komi : float
-            komi used for the current game
-        board: GoBoard
-            SIZExSIZE array representing the current board state
+        debug_mode: prints debug messages
         """
-        mode='w'
         self.stdout = sys.stdout
-        #sys.stdout = outfile
-        self._debug_mode = debug_mode
-        self.file = open(outfile, mode)
-        #self.stderr = sys.stderr
         sys.stdout = self
+        self._debug_mode = debug_mode
         self.go_engine = go_engine
         self.komi = 0
         self.board = GoBoard(7)
+        self.timelimit = 1
+        self.toPlay = 'b'
         self.commands = {
             "protocol_version": self.protocol_version_cmd,
             "quit": self.quit_cmd,
@@ -52,7 +48,10 @@ class GtpConnection():
             "list_commands": self.list_commands_cmd,
             "play": self.play_cmd,
             "final_score": self.final_score_cmd,
-            "legal_moves": self.legal_moves_cmd
+            "legal_moves": self.legal_moves_cmd,
+            "timelimit" : self.timeLimit_cmd,
+            "solve" : self.solve_cmd
+
         }
 
         # used for argument checking
@@ -62,27 +61,26 @@ class GtpConnection():
             "komi": (1, 'Usage: komi FLOAT'),
             "known_command": (1, 'Usage: known_command CMD_NAME'),
             "set_free_handicap": (1, 'Usage: set_free_handicap MOVE (e.g. A4)'),
-            "genmove": (1, 'Usage: genmove {w,b}'),
-            "play": (2, 'Usage: play {b,w} MOVE'),
-            "legal_moves": (1, 'Usage: legal_moves {w,b}')
+            "genmove": (1, 'Usage: genmove {w, b}'),
+            "play": (2, 'Usage: play {b, w} MOVE'),
+            "legal_moves": (1, 'Usage: legal_moves {w, b}'),
+            "timelimit": (1, 'Usage: timelimit INT')
+
         }
     
     def __del__(self):
         sys.stdout = self.stdout
-        self.file.close()
 
     def write(self, data):
-        self.file.write(data)
-        self.stdout.write(data) 
+        self.stdout.write(data)
 
-    def flush(self,):
+    def flush(self):
         self.stdout.flush()
-        self.file.flush()
 
     def start_connection(self):
         """
-        start a GTP connection. This function is what continuously monitors the user's
-        input of commands.
+        start a GTP connection. This function is what continuously monitors
+        the user's input of commands.
         """
         self.debug_msg("Start up successful...\n\n")
         line = sys.stdin.readline()
@@ -111,7 +109,12 @@ class GtpConnection():
         if not elements:
             return
         command_name = elements[0]; args = elements[1:]
-        if self.arg_error(elements, len(args)):
+        
+        if command_name == "play" and self.argmap[command_name][0] != len(args):
+            self.respond('illegal move: {} wrong number of arguments'.format(args[0]))
+            return
+
+        if self.arg_error(command_name, len(args)):
             return
         if command_name in self.commands:
             try:
@@ -125,9 +128,7 @@ class GtpConnection():
             self.error('Unknown command')
             sys.stdout.flush()
 
-    def arg_error(self, cmd, argnum): 
-    #Assumes "illegal arguments" only matters
-    #For play move, as per example. Left the usage as is
+    def arg_error(self, cmd, argnum):
         """
         checker funciton for the number of arguments given to a command
 
@@ -143,27 +144,21 @@ class GtpConnection():
         True if there was an argument error
         False otherwise
         """
-        command = cmd[0]
-        if command in self.argmap and self.argmap[command][0] > argnum:
-                if len(cmd) > 1:
-                    self.respond("illegal move: %s wrong number of arguments" % cmd[1])
-                elif cmd[0] == "play":
-                    self.respond("illegal move: %s wrong number of arguments" % cmd[0])
-                else:
-                    self.error(self.argmap[command][1])
-                return True
+        if cmd in self.argmap and self.argmap[cmd][0] > argnum:
+            self.error(self.argmap[cmd][1])
+            return True
         return False
 
-    def debug_msg(self, msg=''):
+    def debug_msg(self, msg = ''):
         """ Write a msg to the debug stream """
         if self._debug_mode:
             sys.stderr.write(msg); sys.stderr.flush()
 
-    def error(self, error_msg=''):
+    def error(self, error_msg = ''):
         """ Send error msg to stdout and through the GTP connection. """
         sys.stdout.write('? {}\n\n'.format(error_msg)); sys.stdout.flush()
 
-    def respond(self, response=''):
+    def respond(self, response = ''):
         """ Send msg to stdout """
         sys.stdout.write('= {}\n\n'.format(response)); sys.stdout.flush()
 
@@ -275,8 +270,8 @@ class GtpConnection():
         """
         try:
             board_color = args[0].lower()
-            color= GoBoardUtil.color_to_int(board_color)
-            moves=GoBoardUtil.generate_legal_moves(self.board,color)
+            color = GoBoardUtil.color_to_int(board_color)
+            moves = GoBoardUtil.generate_legal_moves(self.board, color)
             self.respond(moves)
         except Exception as e:
             self.respond('Error: {}'.format(str(e)))
@@ -298,39 +293,28 @@ class GtpConnection():
         try:
             board_color = args[0].lower()
             board_move = args[1]
-            if (board_color !='b'):
-            	if board_color != 'w':
-            			self.respond("illegal move: %s %s wrong color" %(board_color,args[1]))
-            			return
-            color= GoBoardUtil.color_to_int(board_color,board_move)
-            if args[1].lower()=='pass':
-                self.debug_msg("Player {} is passing\n".format(args[0]))
-                self.respond("illegal move: {}".format(board_move))
-                return
-            move = GoBoardUtil.move_to_coord(args[1], self.board.size,args[0])
-            if move[0] == False:
-            	self.respond(move[1])
-            	return
+            color = GoBoardUtil.color_to_int(board_color)
+            move = GoBoardUtil.move_to_coord(args[1], self.board.size)
             if move:
-                move = self.board._coord_to_point(move[0],move[1])
-            # move == None on pass
+                move = self.board._coord_to_point(move[0], move[1])
             else:
-                self.error("Error in executing the move %s, check given move: %s"%(move,args[1]))
                 return
-            temp,msg = self.board.move(move, color)
-            if temp == False:
-                #self.respond("Illegal Move: Test {}".format(board_move))
-                self.respond("illegal move: " + args[0] +' ' + board_move + " " + msg)
-                return 
+            if not self.board.move(move, color):
+                return
+            if board_color == 'b':    
+                self.toPlay = 'w'
             else:
-                self.debug_msg("Move: {}\nBoard:\n{}\n".format(board_move, str(self.board.get_twoD_board())))
+                self.toPlay = 'b'
             self.respond()
         except Exception as e:
-            self.respond('Error: {}'.format(str(e)))
+            self.respond("illegal move: {} {} {}".format(board_color, board_move, str(e)))
 
     def final_score_cmd(self, args):
         self.respond(self.board.final_score(self.komi))
 
+
+
+    #EDITS HERE TOO
     def genmove_cmd(self, args):
         """
         generate a move for the specified color
@@ -345,14 +329,11 @@ class GtpConnection():
         """
         try:
             board_color = args[0].lower()
+            self.toPlay = board_color
             color = GoBoardUtil.color_to_int(board_color)
-            self.debug_msg("Board:\n{}\nko: {}\n".format(str(self.board.get_twoD_board()),
-                                                          self.board.ko_constraint))
             move = self.go_engine.get_move(self.board, color)
             if move is None:
-                #self.respond("pass")
-                board_move = "passing"
-                self.respond("illegal move: {}".format(board_move))
+                self.respond("resign")
                 return
 
             if not self.board.check_legal(move, color):
@@ -362,12 +343,40 @@ class GtpConnection():
                 raise RuntimeError("Illegal move given by engine")
 
             # move is legal; play it
-            self.board.move(move,color)
-
+            self.board.move(move, color)
             self.debug_msg("Move: {}\nBoard: \n{}\n".format(move, str(self.board.get_twoD_board())))
             move = self.board._point_to_coord(move)
             board_move = GoBoardUtil.format_point(move)
+
+            #Changes to play to the next player
+            if self.toPlay == 'b':
+                self.toPlay = 'w'
+            else:
+                self.toPlay = 'b'
             self.respond(board_move)
         except Exception as e:
             self.respond('Error: {}'.format(str(e)))
+
+
+
+    #COPY PASTA
+    def timelimit_cmd(self, args):
+        self.timelimit = int(args[0])
+        self.respond()
+
+    def solve_cmd(self,args):
+        win = solve()
+        if win == 'unknown':
+            self.respond('unknown')
+        else:
+            self.respond(win)
+    def solve(self):
+        start = time.process_time()
+        timeUsed = 0
+        while(timeUsed < self.timelimit):
+            timeUsed = time.process_time() - start
+            #Do things
+        return("unknown")
+
+    
 
